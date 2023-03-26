092a1ad10b68e84410adc2772cf64ed62053d76c
Uday <geareab.studios@gmail.com>
2023-03-26 16:46:37 +0000

    mongo bug fix

--- backend/app.js
+++ backend/app.js
@@ -39,7 +39,7 @@ app.use((error, req, res, next) => {
 });
 mongoose
   .connect(
-    `mongodb+srv://hitenmittal:mongodbatlasnode@cluster0.ap5u3dr.mongodb.net/test?retryWrites=true&w=majority`
+    `mongodb+srv://hitenmittal:mongodbatlasnode@cluster0.ap5u3dr.mongodb.net/test?authSource=admin&retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(process.env.PORT || 80);
